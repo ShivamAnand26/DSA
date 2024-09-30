b9f1802ebd4aa768e5ad562cdfc554b46aa933aa
ShivamAnand26 <anandshivi26@gmail.com>
2024-09-30 15:28:45 +0530

    No. of Pair of shoes

---
+++ TCS/NoOfShoes.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int countshoespair(vector<string> shoes)
+{
+    int pair = 0;
+    unordered_map<int, int> leftCount, rightCount;
+
+    for (const auto &shoe : shoes)
+    {
+        int size = shoe[0] - '0';
+        int type = shoe[1];
+
+        if (type == 'l')
+        {
+            leftCount[size]++;
+        }
+        else if (type == 'r')
+        {
+            rightCount[size]++;
+        }
+    }
+
+    for (const auto &entry : leftCount)
+    {
+        int size = entry.first;
+        int leftShoes = entry.second;
+        int rightShoes = rightCount[size];
+
+        pair += min(leftShoes, rightShoes);
+    }
+    return pair;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<string> v(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        string s;
+        cin >> s;
+        v[i] = s;
+    }
+
+    int output = countshoespair(v);
+    cout << output;
+}
